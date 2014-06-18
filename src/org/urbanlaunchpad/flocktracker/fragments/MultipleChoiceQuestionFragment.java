69efdb6ad8daf533d7d8d602410ba20858d2496b
buzoherbert <buzoherbert@gmail.com>
2014-06-18 14:09:25 -0700

    removed unused code

--- src/org/urbanlaunchpad/flocktracker/fragments/MultipleChoiceQuestionFragment.java
+++ src/org/urbanlaunchpad/flocktracker/fragments/MultipleChoiceQuestionFragment.java
@@ -1,80 +1,75 @@
 package org.urbanlaunchpad.flocktracker.fragments;
 
-import java.util.ArrayList;
-
-import android.content.Context;
-import android.graphics.Typeface;
 import android.view.*;
 import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-
 import org.urbanlaunchpad.flocktracker.R;
 import org.urbanlaunchpad.flocktracker.models.Question;
 import org.urbanlaunchpad.flocktracker.views.AnswerView;
 
 public class MultipleChoiceQuestionFragment extends QuestionFragment {
 	private AnswerView[] answersLayout;
-  private LinearLayout answersContainer;
-  private int selectedAnswerIndex = -1;
+	private LinearLayout answersContainer;
+	private int selectedAnswerIndex = -1;
 
+	private OnClickListener onClickListener = new OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			// Disable the last clicked one
+			if (selectedAnswerIndex != -1) {
+				answersLayout[selectedAnswerIndex].disable();
+			}
 
-  private OnClickListener onClickListener = new OnClickListener() {
-    @Override
-    public void onClick(View v) {
-      // Disable the last clicked one
-      if (selectedAnswerIndex != -1) {
-        answersLayout[selectedAnswerIndex].disable();
-      }
+			((AnswerView) v).enable();
+			selectedAnswerIndex = v.getId();
+			getListener().onSelectedAnswer(
+					((AnswerView) v).getAnswer().toString());
+		}
+	};
 
-      ((AnswerView) v).enable();
-      selectedAnswerIndex = v.getId();
-      getListener().onSelectedAnswer(((AnswerView) v).getAnswer().toString());
-    }
-  };
+	public MultipleChoiceQuestionFragment(QuestionActionListener listener,
+			Question question, QuestionType questionType) {
+		super(listener, question, questionType);
 
-  public MultipleChoiceQuestionFragment(QuestionActionListener listener, Question question, QuestionType questionType) {
-    super(listener, question, questionType);
+		answersContainer = (LinearLayout) getView().findViewById(
+				R.id.answer_layout);
+	}
 
-    answersContainer = (LinearLayout) getView().findViewById(R.id.answer_layout);
-  }
-
-  public void setupLayout() {
+	public void setupLayout() {
 
 		boolean hasOther = getQuestion().isOtherEnabled();
 		String[] answers = getQuestion().getAnswers();
 		int numAnswers = hasOther ? answers.length : answers.length + 1;
 		answersLayout = new AnswerView[numAnswers];
 
-    // Add listeners for answers
+		// Add listeners for answers
 		for (int i = 0; i < answers.length; i++) {
-      answersLayout[i] = (AnswerView) getInflater().inflate(R.layout.question_answer, null);
-      answersLayout[i].initialize(getQuestion().getType(), answers[i]);
+			answersLayout[i] = (AnswerView) getInflater().inflate(
+					R.layout.question_answer, null);
+			answersLayout[i].initialize(getQuestion().getType(), answers[i]);
 			answersLayout[i].setOnClickListener(onClickListener);
-      answersContainer.addView(answersLayout[i]);
+			answersContainer.addView(answersLayout[i]);
 		}
 
 		if (hasOther) {
-      answersLayout[numAnswers - 1] = (AnswerView) getInflater().inflate(R.layout.question_answer, null);
-      answersLayout[numAnswers - 1].initialize(getQuestion().getType(), null);
-      answersLayout[numAnswers - 1].setOnClickListener(onClickListener);
-      answersContainer.addView(answersLayout[numAnswers - 1]);
+			answersLayout[numAnswers - 1] = (AnswerView) getInflater().inflate(
+					R.layout.question_answer, null);
+			answersLayout[numAnswers - 1].initialize(getQuestion().getType(),
+					null);
+			answersLayout[numAnswers - 1].setOnClickListener(onClickListener);
+			answersContainer.addView(answersLayout[numAnswers - 1]);
 		}
 		prepopulateQuestion();
 	}
 
 	@Override
 	public void prepopulateQuestion() {
-//		if (selectedAnswers != null) {
-//			if (selectedAnswers.get(0) == answersLayout.length) {
-//				EditText answerText = (EditText) answersLayout[answersLayout.length]
-//						.findViewById(ANSWER_TAG);
-//				answerText.setText(jquestion.getString("Answer"));
-//			}
-//		}
+		// if (selectedAnswers != null) {
+		// if (selectedAnswers.get(0) == answersLayout.length) {
+		// EditText answerText = (EditText) answersLayout[answersLayout.length]
+		// .findViewById(ANSWER_TAG);
+		// answerText.setText(jquestion.getString("Answer"));
+		// }
+		// }
 	}
 }
